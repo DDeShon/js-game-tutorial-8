66ea4f6dd2f404289752db0d7440f6b79e08591a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-06-27 12:49:31 -0500

    working on animation

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ class Raven {
   constructor() {
     this.spriteWidth = 271;
     this.spriteHeight = 194;
-    this.width = this.spriteWidth;
-    this.height = this.spriteHeight;
+    this.sizeModifier = Math.random() * 0.6 + 0.4;
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
     this.x = canvas.width;
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
@@ -22,16 +23,26 @@ class Raven {
     this.markedForDeletion = false;
     this.image = new Image();
     this.image.src = "raven.png";
+    this.frame = 0;
+    this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 100 + 40;
   }
-  update() {
+  update(deltaTime) {
     this.x -= this.directionX;
     if (this.x < 0 - this.width) this.markedForDeletion = true;
+    this.timeSinceFlap += deltaTime;
+    if (this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
+    }
   }
   draw() {
-    // ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
       this.image,
-      0,
+      this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
       this.spriteHeight,
@@ -52,10 +63,9 @@ function animate(timestamp) {
     ravens.push(new Raven());
     timeToNextRaven = 0;
   }
-  [...ravens].forEach((object) => object.update());
+  [...ravens].forEach((object) => object.update(deltaTime));
   [...ravens].forEach((object) => object.draw());
   ravens = ravens.filter((object) => !object.markedForDeletion);
-  console.log(ravens);
 
   requestAnimationFrame(animate);
 }
