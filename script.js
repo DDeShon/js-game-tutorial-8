81cedf5eaeb8a404a98fee1e733b3c886199f062
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-04 16:34:47 -0500

    working on particles

--- script.js
+++ script.js
@@ -143,11 +143,13 @@ class Particle {
     if (this.radius > this.maxRadius) this.markedForDeletion = true;
   }
   draw() {
-    ctx.globalAlpha = 1;
+    ctx.save();
+    ctx.globalAlpha = 1 - this.radius / this.maxRadius;
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
+    ctx.restore();
   }
 }
 
